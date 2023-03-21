f9b74823d34a9f14ef62f039d8ae79446a849774
Jessica LIMA PINTO <jessica.lima-pinto@ens.uvsq.fr>
2023-03-21 20:50:01 +0100

    interphase

--- Mastermind.py
+++ Mastermind.py
@@ -67,19 +67,5 @@ canvasligne.create_line(master_mind,x = 400, y = 600, width = 3 #épaisseur de l
    
 
 
-#Interphase
-
-
-
-print("-----------------------------------------")
-print("\t\tMenu")
-print("-----------------------------------------")
-print("Enter code using numbers.")
-print("1 - rouge, 2 - vert, 3 - jaune, 4 - bleu, 5 - blanc, 6 - orange, 7 - violet, 8 - rose")
-print("Example: RED YELLOW ORANGE BLACK ---> 1 3 6 5")
-print("-----------------------------------------")
-print_mastermind_board(show_passcode, guess_codes, guess_flags)
-couleurs = ['rouge', 'rose', 'bleu', 'vert', 'jaune', 'orange', 'violet', 'blanc']
-essais = 10    # nbr d'essai pour trouver le code secret
 
 
