17a588f326ba72f9cef8a579f2291ae06af64736
Jessica LIMA PINTO <jessica.lima-pinto@ens.uvsq.fr>
2023-03-22 22:35:48 +0100

    comm

--- Mastermind/Mastermind.py
+++ Mastermind/Mastermind.py
@@ -1,7 +1,7 @@
-from tkinter import CENTER, RIGHT, Frame, Label, Menu, Tk, Toplevel, ttk
+from tkinter import CENTER, RIGHT, Frame, Label, Menu, Tk, Toplevel, ttk, Canvas, Button
 import tkinter
 from tkinter.messagebox import showinfo
-
+from PIL import ImageTk, Image
 
 
 # Paramètres fenetre racine
@@ -88,10 +88,11 @@ bouton1.pack(side = 'left', fill='x', expand = True)            # .pack = pour a
 def bouton2fonction () :
     bouton2=Toplevel(racine)
     bouton2.title("Mode 2 joueurs")
-    bouton2.geometry("600x600")
+    bouton2.geometry("400x700") #message de jessica, j'ai agrandit la fenetre 
     bouton2["bg"] = 'oldlace'
     labelbouton2 = Label(bouton2, text = 'Mode 2 joueurs', fg = ("black"), bg=("oldlace"), font =("helvetica", "15"))
     labelbouton2.pack (side = "top")
+    
 
     # Création Menu
     bouton2_menu = Menu(bouton2)               
@@ -102,6 +103,7 @@ def bouton2fonction () :
     main_cascade.add_separator()
     main_cascade.add_separator()
     main_cascade.add_command(label='À propos', command = aproposbouton2)
+
  
 
 def preferencebouton2 () :
@@ -150,101 +152,6 @@ racine.mainloop()      # pour que le fenetre reste ouverte, boucle infinie
 
 
 
-from tkinter import Tk, Button, Canvas, Label, Frame, Text
-from PIL import ImageTk, Image
 
-
-
-#Interphase
-# en cours de programation 
-#création de la fenetre master mind
-
-#création du bouton validé et supprimer
-bvalide = Button(racine,text = 'Validé')
-bsupprimer = Button(racine,text = 'Supprimer')
-
-#placement des boutons validé et supprimer
-bvalide.place(x = 200, y = 580)
-bsupprimer.place(x = 190, y = 610)
-
-#création de la ligne du bas
-ligne1=Canvas(racine,width=400, height=30)#largeur et hauteur du canvas 
-a=(0, 30)
-b=(400,30)
-ligne1.create_line(a, b)
-ligne1.place(x = 20, y = 470)#placer le canvas
-
-#création de la ligne du haut
-ligne1=Canvas(racine,width=400, height=30)#largeur et hauteur du canvas 
-a=(0, 30)
-b=(400,30)
-ligne1.create_line(a, b)
-ligne1.place(x = 20, y = 30)
-
-
-
-
-#creation de la frame et des boutons de couleurs 
-frameb1 = Frame(racine, width=400, height=600, borderwidth=2)
-frameb1.place(x= 60, y=535)
-
-#ouvrir les images 
-img = Image.open('boutonrose.PNG')
-img1 = Image.open('boutonviolet.PNG')
-img2 = Image.open('boutonbleuf.PNG')
-img3 = Image.open('boutonbleuciel.PNG')
-img4 = Image.open('boutonturquoise.PNG')
-img5 = Image.open('boutonjeune.PNG')
-img6 = Image.open('boutonorange1.PNG')
-img7 = Image.open('boutonrouge.PNG')
-#redimentionner les images
-taille0= img.resize((35,35))
-a = ImageTk.PhotoImage(taille0)
-
-taille1 = img1.resize((35,35))
-b = ImageTk.PhotoImage(taille1)
-
-taille2 = img2.resize((35,35))
-c = ImageTk.PhotoImage(taille2)
-
-taille3 = img3.resize((35,35))
-d = ImageTk.PhotoImage(taille3)
-
-taille4 = img4.resize((35,35))
-e = ImageTk.PhotoImage(taille4)
-
-taille5 = img5.resize((35,35))
-f = ImageTk.PhotoImage(taille5)
-
-taille6 = img6.resize((35,35))
-g = ImageTk.PhotoImage(taille6)
-
-taille7 = img7.resize((35,35))
-h = ImageTk.PhotoImage(taille7)
-#bouton bleu ciel
-B0 = Button(frameb1, image=a)
-B1 = Button(frameb1, image=b)
-B2 = Button(frameb1, image=c)
-B3 = Button(frameb1, image=d)
-B4 = Button(frameb1, image=e)
-B5 = Button(frameb1, image=f)
-B6 = Button(frameb1, image=g)
-B7 = Button(frameb1, image=h)
-#affichage des boutons
-B0.grid()
-B1.grid(row =0, column=1)
-B2.grid(row =0,column=2)
-B3.grid(row =0,column=3)
-B4.grid(row =0,column=4)
-B5.grid(row =0,column=5)
-B6.grid(row =0,column=6)
-B7.grid(row =0,column=7)
-# création d'une nouvelle frame pour les grilles d'esssais 
-
-frameb2 = Frame(bouton2, width=400, height=600, borderwidth=2)
-frameb2.pack
-
-
-palette.place(x=170, y=509)
 
 
