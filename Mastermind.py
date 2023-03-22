c05f3c95cea73595dd49271527405f1a6c2c1d0a
uvsq22200131 <113447634+uvsq22200131@users.noreply.github.com>
2023-03-22 11:31:38 +0100

    Update Mastermind.py

--- Mastermind.py
+++ Mastermind.py
@@ -1,6 +1,6 @@
-from tkinter import CENTER, RIGHT, Frame, Label, Tk, Toplevel, Canvas,PhotoImage, Frame, Button
+from tkinter import CENTER, RIGHT, Frame, Label, Menu, Tk, Toplevel, ttk
 import tkinter
-from PIL import ImageTk, Image
+from tkinter.messagebox import showinfo
 
 
 
@@ -10,37 +10,96 @@ racine.geometry("600x600")    # régler la taille de la fenetre
 racine.title("Mastermind")     # nom fenetre
 racine["bg"] = 'pink3'      # couleur arrière plan 
 racine.resizable(height=False, width=False)   # = fenetre pas redimensionnable dans longeur et largeur, figer les dimenssions
-racine.grid_columnconfigure(4, weight=4)        # C'EST QUOIIIIIIIII
 
 label = Label(racine, text="Bienvenue", fg = ("black"), bg=("pink3"), font =("helvetica", "25"))
 label.pack(side="top")
+
 label1 = Label(racine, text="Choisissez un mode", fg = ("black"), bg=("pink3"), font =("helvetica", "14"))
-label1.place(x='235', y='220')
+label1.place(x='210', y='220')
 
 
 
 # Création boutons 
 # Fonction bouton 1
-def bouton1fonction ():                     # Code de chez StackLima.com
-    bouton1=Toplevel(racine)
+def bouton1fonction ():                     
+    bouton1=Toplevel(racine)                # touver chez StackLima.com
     bouton1.title("Mode 1 joueur")
     bouton1.geometry("600x600")
     bouton1["bg"] = 'rosybrown1'
     labelbouton1 = Label(bouton1, text = 'Mode 1 joueur', fg = ("black"), bg=("rosybrown1"), font =("helvetica", "15"))
     labelbouton1.pack (side = "top")
+
+    # Création Menu
+    bouton1_menu = Menu(bouton1)               
+    bouton1['menu'] = bouton1_menu
+    main_cascade = Menu(bouton1_menu)
+    bouton1_menu.add_cascade(label='Menu', menu = main_cascade)
+    main_cascade.add_command(label='Préférences', command = preferencebouton1)
+    main_cascade.add_separator()
+    main_cascade.add_separator()
+    main_cascade.add_command(label='À propos', command = aproposbouton1)
+
+nombrecouleurs = ["4", "5", "6", "7", "8"]
+nombrepions = ["3", "4", "5", "6", "7", "8"]
+
+def preferencebouton1 () :
+    preference = Toplevel(bouton1)
+    preference.title ("Préferences")
+    preference.geometry ("300x300")
+    labelpreference1 = Label (preference, text = 'Choississez un nombre de pions', fg = ('black'), font = ('helvetica', '10'))
+    labelpreference1.pack(side ='top')
+    listeCombo1 = ttk.Combobox (preference, values = nombrecouleurs)
+    listeCombo1.pack()
+    labelpreference2 = Label (preference, text = 'Choississez un nombre de couleurs', fg = ('black'), font = ('helvetica', '10'))
+    labelpreference2.pack(side = 'top')
+    listeCombo2 = ttk.Combobox (preference, values = nombrecouleurs)
+    listeCombo2.pack()
+    
+
+def aproposbouton1 () :             #https://www.invivoo.com/realiser-mastermind-tkinter-python-part-3/
+    showinfo ('À propos',
+             message = "Bienvenue dans Mastermind.\n\n"
+                     "Ce jeu consiste à trouver un code secret composé de plusieurs couleurs, sachant que "
+                     "chaque couleur peut apparaître plusieurs fois.\n\n"
+                     "Vous pouvez configurer le nombre de couleurs différentes, la taille du code secret "
+                     "et le nombre de tentatives que vous pouvez effectuer dans le menu Préférences.")
+       
 # Bouton mode 1 joueur 
 bouton1 = tkinter.Button(racine, text = 'mode 1 joueur', bd = '5', command=bouton1fonction)
 bouton1.pack(side = 'left', fill='x', expand = True)            # .pack = pour afficher le bouton       SAVOIR EXPLIQUER LE RESTE 
 
 
+
+
 # Fonction bouton 2 
 def bouton2fonction () :
     bouton2=Toplevel(racine)
-    bouton2.title("Mode 1 joueur")
+    bouton2.title("Mode 2 joueurs")
     bouton2.geometry("600x600")
     bouton2["bg"] = 'rosybrown1'
     labelbouton2 = Label(bouton2, text = 'Mode 2 joueurs', fg = ("black"), bg=("rosybrown1"), font =("helvetica", "15"))
-    labelbouton2.pack (side = "top")
+    labelbouton2.place (side = "top")
+
+    # Création Menu
+    bouton2_menu = Menu(bouton1)               
+    bouton2['menu'] = bouton2_menu
+    main_cascade = Menu(bouton2_menu)
+    bouton2_menu.add_cascade(label='Menu', menu = main_cascade)
+    main_cascade.add_command(label='Préférences', command = preferencebouton2)
+    main_cascade.add_separator()
+    main_cascade.add_separator()
+    main_cascade.add_command(label='À propos', command = aproposbouton2)
+
+def preferencebouton2 () :
+    print ("bonjour")
+
+def aproposbouton2 () :             #https://www.invivoo.com/realiser-mastermind-tkinter-python-part-3/
+    showinfo ('À propos',
+             message="Bienvenue dans Mastermind.\n\n"
+                     "Ce jeu consiste à trouver un code secret composé de plusieurs couleurs, sachant que "
+                     "chaque couleur peut apparaître plusieurs fois.\n\n"
+                     "Vous pouvez configurer le nombre de couleurs différentes, la taille du code secret "
+                     "et le nombre de tentatives que vous pouvez effectuer dans le menu Préférences.")
 
 # Bouton mode 2 joueurs 
 bouton2 = tkinter.Button(racine, text = 'mode 2 joueurs', bd = '5', command=bouton2fonction)          # On peut aussi mettre un bg... comme sur les labels
@@ -49,7 +108,12 @@ bouton2.pack(side = 'left', fill='x', expand = True)
 
 
 
-racine.mainloop()  # pour que le fenetre reste ouverte, boucle infinie
+
+
+racine.mainloop()      # pour que le fenetre reste ouverte, boucle infinie
+
+
+
 
 
 #Interphase
@@ -81,4 +145,3 @@ ligne2.place(x = 320, y = 9)
 
 
 master_mind.mainloop()
-
