d1ffddbf857bc0406135f4070ec89f5ce8a28209
Jessica LIMA PINTO <jessica.lima-pinto@ens.uvsq.fr>
2023-03-24 12:19:08 +0100

    commit jess

--- Mastermind/Mastermind.py
+++ Mastermind/Mastermind.py
@@ -21,22 +21,21 @@ M.place(x = 230, y=50)
 
 
 
-# Création boutons 
+# CREATION BOUTON 
 nombrecouleurs = ["4", "5", "6", "7", "8"]
 nombrepions = ["3", "4", "5", "6", "7", "8"]
 nombreessai = ["6", "7", "8", "9", "10", "15", "20"]
-# Fonction bouton 1
 
+# Fonction bouton 1
 def bouton1fonction ():                     
     master_mind=Toplevel(racine)
-    
     master_mind.geometry("450x660")#taille de la fenetre
-
     master_mind.title("Master mind mode 1 joueurs")#titre de la fenetre 
     palette = Label(master_mind, text='palette de couleurs :') 
     c = Label(master_mind, text='trouver le code ')
     s = Label(master_mind,text='secret ;)')
-     # Création Menu
+
+    # Création Menu
     bouton1_menu = Menu(master_mind)               
     master_mind['menu'] = bouton1_menu
     main_cascade = Menu(bouton1_menu)
@@ -45,19 +44,20 @@ def bouton1fonction ():
     main_cascade.add_separator()
     main_cascade.add_separator()
     main_cascade.add_command(label='À propos', command = aproposbouton1)
+    main_cascade.add_separator()
+    main_cascade.add_separator()
+    main_cascade.add_command(label='Ajouter un timer', command = timerbouton1)
 
-
-
-   
     #création du bouton validé, supprimer et quitter 
     bvalide = Button(master_mind,text = 'Validé', height=1, width=8)
     bsupprimer = Button(master_mind,text = 'Supprimer',height=1, width=8)
     bquitter = Button(master_mind,text = 'Quitter',height=1, width=8,command=master_mind.destroy)
-
+    bsauvegarder = Button(master_mind,text = 'Sauvegarder', height=1, width=8)
     #placement des boutons validé,supprimer,quitter et le label code
-    bvalide.place(x = 175, y = 600)
-    bsupprimer.place(x =50, y = 600)
-    bquitter.place(x = 300, y = 600)
+    bvalide.place(x = 175, y = 585)
+    bsupprimer.place(x =50, y = 585)
+    bquitter.place(x = 300, y = 585)
+    bsauvegarder.place(x =175, y=620)
     c.place(x= 5, y=5)
     s.place(x=20, y=23)
 
@@ -69,10 +69,8 @@ def bouton1fonction ():
     ligne1.place(x = 20, y = 470)#placer le canvas
 
     #création du canvas qui va révéler le code secret  
-
     canvascs = Canvas(master_mind,width = 200, height = 40)
     canvascs.place(x = 100, y= 8) 
-
     canvascs.create_oval(50,10,80,40)
     canvascs.create_oval(90,10,120,40)
     canvascs.create_oval(130,10,160,40)
@@ -84,8 +82,6 @@ def bouton1fonction ():
     b=(400,30)
     ligne1.create_line(a, b)
     ligne1.place(x = 20, y = 50)
-
-
 
     #creation de la frame et des boutons de couleurs 
     frameb1 = Frame(master_mind, width=400, height=600, borderwidth=2)
@@ -100,6 +96,7 @@ def bouton1fonction ():
     img5 = Image.open('boutonjeune.PNG')
     img6 = Image.open('boutonorange1.PNG')
     img7 = Image.open('boutonrouge.PNG')
+
     #redimentionner les images
     taille0= img.resize((35,35))
     a = ImageTk.PhotoImage(taille0)
@@ -124,7 +121,8 @@ def bouton1fonction ():
 
     taille7 = img7.resize((35,35))
     h = ImageTk.PhotoImage(taille7)
-    #bouton bleu ciel
+
+    #boutons
     B0 = Button(frameb1, image=a)
     B1 = Button(frameb1, image=b)
     B2 = Button(frameb1, image=c)
@@ -133,6 +131,7 @@ def bouton1fonction ():
     B5 = Button(frameb1, image=f)
     B6 = Button(frameb1, image=g)
     B7 = Button(frameb1, image=h)
+
     #affichage des boutons
     B0.grid()
     B1.grid(row =0, column=1)
@@ -142,20 +141,6 @@ def bouton1fonction ():
     B5.grid(row =0,column=5)
     B6.grid(row =0,column=6)
     B7.grid(row =0,column=7)
-
-    # Création Menu
-    bouton1_menu = Menu(bouton1)               
-    bouton1['menu'] = bouton1_menu
-    main_cascade = Menu(bouton1_menu)
-    bouton1_menu.add_cascade(label='Menu', menu = main_cascade)
-    main_cascade.add_command(label='Préférences', command = preferencebouton1)
-    main_cascade.add_separator()
-    main_cascade.add_separator()
-    main_cascade.add_command(label='À propos', command = aproposbouton1)
-
-nombrecouleurs = ["4", "5", "6", "7", "8"]
-nombrepions = ["3", "4", "5", "6", "7", "8"]
-nombreessai = ["6", "7", "8", "9", "10", "15", "20"]
 
 
 def preferencebouton1 () :
@@ -171,7 +156,7 @@ def preferencebouton1 () :
     labelpreference2.pack(side = 'top')
     listeCombo2 = ttk.Combobox (preference, values = nombrecouleurs)
     listeCombo2.pack()
-    labelpreference3 = Label (preference, text = 'Choississez un nombre d esssai', fg = ('black'), bg = ('oldlace'), font = ('helvetica', '10'))
+    labelpreference3 = Label (preference, text = "Choississez un nombre d' esssai", fg = ('black'), bg = ('oldlace'), font = ('helvetica', '10'))
     labelpreference3.pack(side = 'top')
     listeCombo3 = ttk.Combobox (preference, values = nombreessai)
     listeCombo3.pack()
@@ -187,9 +172,15 @@ def aproposbouton1 () :
                      "Vous pouvez configurer le nombre de couleurs différentes, la taille du code secret "
                      "et le nombre de tentatives que vous pouvez effectuer dans le menu Préférences.")
     
+
 def appliquerparametres1 () : 
     print ("boujour")
-   
+
+
+def timerbouton1 () :
+    print ('Bonjour')       
+
+
 # Bouton mode 1 joueur 
 bouton1 = tkinter.Button(racine, text = 'mode 1 joueur', bd = '5', command=bouton1fonction)
 bouton1.pack(side = 'left', fill='x', expand = True)            # .pack = pour afficher le bouton       SAVOIR EXPLIQUER LE RESTE 
@@ -201,40 +192,41 @@ bouton1.pack(side = 'left', fill='x', expand = True)            # .pack = pour a
 # Fonction bouton 2 
 def bouton2fonction () :
     master_mind=Toplevel(racine)
-    
     master_mind.geometry("450x660")#taille de la fenetre
-
     master_mind.title("Master mind mode 2 joueurs")#titre de la fenetre 
     palette = Label(master_mind, text='palette de couleurs :')
     c = Label(master_mind, text='trouver le code ')
     s = Label(master_mind,text='secret ;)') 
-     # Création Menu
-    bouton1_menu = Menu(master_mind)               
-    master_mind['menu'] = bouton1_menu
-    main_cascade = Menu(bouton1_menu)
-    bouton1_menu.add_cascade(label='Menu', menu = main_cascade)
-    main_cascade.add_command(label='Préférences', command = preferencebouton1)
+
+    # Création Menu
+    bouton2_menu = Menu(master_mind)               
+    master_mind['menu'] = bouton2_menu
+    main_cascade = Menu(bouton2_menu)
+    bouton2_menu.add_cascade(label='Menu', menu = main_cascade)
+    main_cascade.add_command(label='Préférences', command = preferencebouton2)
     main_cascade.add_separator()
     main_cascade.add_separator()
-    main_cascade.add_command(label='À propos', command = aproposbouton1)
+    main_cascade.add_command(label='À propos', command = aproposbouton2)
+    main_cascade.add_separator()
+    main_cascade.add_separator()
+    main_cascade.add_command(label='Ajouter un timer', command = timerbouton2)
 
     #création du bouton validé,supprimer et quitter
     bvalide = Button(master_mind,text = 'Validé', height=1, width=8)
     bsupprimer = Button(master_mind,text = 'Supprimer',height=1, width=8)
     bquitter = Button(master_mind,text = 'Quitter',height=1, width=8,command=master_mind.destroy)
-    
-
+    bsauvegarder = Button(master_mind,text= 'Sauvegarder', height=1, width=8)
     #placement des boutons validé,supprimer,quiter + label
-    bvalide.place(x = 175, y = 600)
-    bsupprimer.place(x =50, y = 600)
-    bquitter.place(x = 300, y = 600)
+    bvalide.place(x = 175, y = 585)
+    bsupprimer.place(x =50, y = 585)
+    bquitter.place(x = 300, y = 585)
+    bsauvegarder.place(x =175, y=620)
     c.place(x= 5, y=5)
     s.place(x=20, y=23)
 
     #création du canvas qui va révéler le code secret  
     canvascs = Canvas(master_mind,width = 300, height = 40)
     canvascs.place(x = 100, y= 8) 
-
     canvascs.create_oval(50,10,80,40)
     canvascs.create_oval(90,10,120,40)
     canvascs.create_oval(130,10,160,40)
@@ -254,9 +246,6 @@ def bouton2fonction () :
     ligne1.create_line(a, b)
     ligne1.place(x = 20, y = 50)
 
-
-
-
     #creation de la frame et des boutons de couleurs 
     frameb1 = Frame(master_mind, width=400, height=600, borderwidth=2)
     frameb1.place(x= 60, y=535)
@@ -270,6 +259,7 @@ def bouton2fonction () :
     img5 = Image.open('boutonjeune.PNG')
     img6 = Image.open('boutonorange1.PNG')
     img7 = Image.open('boutonrouge.PNG')
+
     #redimentionner les images
     taille0= img.resize((35,35))
     a = ImageTk.PhotoImage(taille0)
@@ -294,7 +284,8 @@ def bouton2fonction () :
 
     taille7 = img7.resize((35,35))
     h = ImageTk.PhotoImage(taille7)
-    #bouton bleu ciel
+
+    #boutons
     B0 = Button(frameb1, image=a)
     B1 = Button(frameb1, image=b)
     B2 = Button(frameb1, image=c)
@@ -303,6 +294,7 @@ def bouton2fonction () :
     B5 = Button(frameb1, image=f)
     B6 = Button(frameb1, image=g)
     B7 = Button(frameb1, image=h)
+
     #affichage des boutons
     B0.grid()
     B1.grid(row =0, column=1)
@@ -312,26 +304,11 @@ def bouton2fonction () :
     B5.grid(row =0,column=5)
     B6.grid(row =0,column=6)
     B7.grid(row =0,column=7)
+
     # création d'une nouvelle frame pour les grilles d'esssais 
-
-
-
     palette.place(x=170, y=509)
 
-
-
     master_mind.mainloop()
-
-    # Création Menu
-    bouton2_menu = Menu(bouton2)               
-    bouton2['menu'] = bouton2_menu
-    main_cascade = Menu(bouton2_menu)
-    bouton2_menu.add_cascade(label='Menu', menu = main_cascade)
-    main_cascade.add_command(label='Préférences', command = preferencebouton2)
-    main_cascade.add_separator()
-    main_cascade.add_separator()
-    main_cascade.add_command(label='À propos', command = aproposbouton2)
-
  
 
 def preferencebouton2 () :
@@ -347,7 +324,7 @@ def preferencebouton2 () :
     labelpreference5.pack(side = 'top')
     listeCombo5 = ttk.Combobox (preference2, values = nombrecouleurs)
     listeCombo5.pack()
-    lebelpreference6 = Label (preference2, text = 'Choississez un nombre d essai', fg = ('black'), bg = ('oldlace'), font = ('helvetica', '10'))
+    lebelpreference6 = Label (preference2, text = "Choississez un nombre d' essai", fg = ('black'), bg = ('oldlace'), font = ('helvetica', '10'))
     lebelpreference6.pack(side = 'top')
     listeCombo6 = ttk.Combobox (preference2, values = nombreessai)
     listeCombo6.pack()
@@ -363,8 +340,13 @@ def aproposbouton2 () :
                      "Vous pouvez configurer le nombre de couleurs différentes, la taille du code secret "
                      "et le nombre de tentatives que vous pouvez effectuer dans le menu Préférences.")
     
+
 def appliquerparametres2 () :
     print ("bonjour")
+
+
+def timerbouton2 () :
+    print ('Bonjour')    
 
 
 # Bouton mode 2 joueurs 
